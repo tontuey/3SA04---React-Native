4f82bf9de92011d598551b668210bd6f81c1b8fa
tontuey <tueyconan@hotmail.co.th>
2020-09-29 20:01:35 +0700

    update App.js and add ZipCodeScreen

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Weather from './components/Weather';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './components/ZipCodeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-
-  return (
-    <View style={styles.container}>
-      <Weather zipCode="90110" />
-      <StatusBar style="auto" />
-    </View>
+  return (        
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+      </Stack.Navigator> 
+    </NavigationContainer>       
   );
-
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    //alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-})
